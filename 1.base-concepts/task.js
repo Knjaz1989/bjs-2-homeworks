2f32c7c393368a1b4102c6c90013d69992a57ccb
Igor Sverchkov <knjaz1989@gmail.com>
2023-08-06 11:48:21 +0300

    task 1 finished

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant === 0) {
+    let sqr = -b / (2 * a);
+    arr.push(sqr);
+  } else if (discriminant > 0) {
+    let sqrOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let sqrTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(sqrOne, sqrTwo);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let list = [percent, contribution, amount, countMonths];
+  for (let index = 0; index < list.length; index++) {
+    if (isNaN(Number(list[index]))) {
+      return false;
+    } 
+  };
+  let persentPerMonth = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let payment = creditBody * (persentPerMonth + (persentPerMonth / (((1 + persentPerMonth) ** countMonths) - 1)));
+  let totalSum = payment * countMonths;
+  return +totalSum.toFixed(2);
 }
