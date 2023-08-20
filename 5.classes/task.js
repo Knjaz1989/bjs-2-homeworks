d88bc6e3573c257611139722f2bfcd8ba532d72e
Igor Sverchkov <knjaz1989@gmail.com>
2023-08-20 14:40:15 +0300

    task 5 finished

---
+++ 5.classes/task.js
@@ -0,0 +1,141 @@
+// task 1
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount, state = 100) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this._state = state;
+		this.type = null;
+	}
+
+	fix() {
+		this._state *= 1.5;
+		if (this._state > 100) {
+			this._state = 100;
+		}
+	}
+
+	set state(bookState) {
+		if (this._state > 100) {
+			this._state = 100;
+		} else if (this._state < 0) {
+			this._state = 0;
+		} else {
+			this._state = bookState;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}
+}
+
+
+class Magazine extends PrintEditionItem {
+	constructor(...args) {
+		super(...args);
+		this.type = "magazine";
+	}
+}
+
+
+class Book extends PrintEditionItem {
+	constructor(author, ...args) {
+		super(...args);
+		this.author = author;
+		this.type = "book";
+	}
+}
+
+
+class NovelBook extends Book {
+	constructor(...args) {
+		super(...args);
+		this.type = "novel";
+	}
+}
+
+
+class FantasticBook extends Book {
+	constructor(...args) {
+		super(...args);
+		this.type = "fantastic";
+	}
+}
+
+
+class DetectiveBook extends Book {
+	constructor(...args) {
+		super(...args);
+		this.type = "detective";
+	}
+}
+
+
+// task 2
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+		let findBook = this.books.find(book => book[type] === value);
+		if (!findBook) {
+			return null;
+		}
+		return findBook;
+	}
+
+	giveBookByName(bookName) {
+		let giveBook = this.findBookBy("name", bookName);
+		if (!giveBook) {
+			return null;
+		}
+		this.books.splice(this.books.indexOf(giveBook), 1);
+		return giveBook;
+	}
+}
+
+
+// task 3
+class Student {
+	constructor(name) {
+		this.name = name;
+		this.marks = {};
+	}
+
+	addMark(mark, subject) {
+		if (1 < mark && mark < 6) {
+			this.marks[subject] ? this.marks[subject].push(mark) : this.marks[subject] = [mark];
+		}
+	}
+
+	getAverageBySubject(subject) {
+		const subjectMarks = this.marks[subject];
+		if (!subjectMarks) {
+			return 0;
+		}
+		let marksSum = subjectMarks.reduce(
+				(acc, value) => acc + value, 0
+			)
+		return marksSum / subjectMarks.length;
+	}
+
+	getAverage() {
+		const subjects = Object.keys(this.marks);
+		if (subjects.length === 0) {
+			return 0;
+		}
+		let allMarksSum = subjects.reduce(
+			(acc, value) => acc + this.getAverageBySubject(value) , 0
+		);
+		return allMarksSum / subjects.length;
+	}
+}
